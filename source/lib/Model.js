c5abe05993b541b223fb0d71df989366ed662e3a
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2016-10-07 20:36:32 +1100

    Tidied up model mapping

--- source/lib/Model.js
+++ source/lib/Model.js
@@ -8,57 +8,30 @@ class Model {
   getModel(item, locale = null, maxDepth = 10) {
     locale = locale || this.config.defaultLocale
 
-    let visitedIds = {}
+    const extractLocale = (item) => {
+      return _.mapValues(item.fields, (value, key, object) => {
+        let ret = null
 
-    const handleObject = (value, level) => {
-      if (typeof(value) === 'object' && value !== null) {
-        if (value.hasOwnProperty('fields')) {
-          if (value.sys && value.sys.id) {
-            if (visitedIds[value.sys.id]) {
-              return value
-            }
-            visitedIds[value.sys.id] = value
-          }
-          value.fields = extractLocale(value.fields, level + 1)
-          if (value.sys && value.sys.id) {
-            visitedIds[value.sys.id] = value
-          }
-        }
-        else {
-          value = extractLocale(value, level + 1)
-        }
-      }
-
-      return value
-    }
-
-    const extractLocale = (fields, level = 0) => {
-      if (level > maxDepth) {
-        return fields
-      }
-
-      return _.mapValues(fields, value => {
         if (typeof(value) === 'undefined' || value === null) {
-          return null
+          ret = null
+        }
+        else if (locale in value) {
+           ret = value[locale]
         }
 
-        if (value instanceof Array) {
-          return _.map(value, ent => ({
-              ...extractLocale(ent, level).fields,
-              $item: ent
-            }))
+        if (ret instanceof Array) {
+          ret = _.map(ret, ent => ({ ...extractLocale(ent), $item: ent }))
+        }
+        else if (ret.hasOwnProperty('fields')) {
+          ret = { ...extractLocale(ret), $item: ret }
         }
 
-        if (typeof(value) === 'object' && value.hasOwnProperty(locale)) {
-          value = value[locale]
-        }
-
-        return handleObject(value, level)
+        return ret
       })
     }
 
     return {
-      ...extractLocale(item, 0).fields,
+      ...extractLocale(item),
       $item: item
     }
   }
